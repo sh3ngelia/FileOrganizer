be6c0f03aa50a2f8438bd1cf08bff80539149a0f
Davit <davitshengelia21@gmail.com>
2025-08-14 12:21:27 +0400

    Add project files.

---
+++ FileOrganizer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FileOrganizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
